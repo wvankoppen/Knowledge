d70f3dfddb25b0b62cafc2cc4dbfe8b29d0dfda5
Wouter van Koppen <wvkoppen@gmail.com>
2022-10-31 17:16:39 +0100

    remove need for index.js

--- JavaScript/webpack.config.js
+++ JavaScript/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const glob = require('glob');
 
 module.exports = {
-    entry: './src/index.js',
+    entry:  {
+        js: glob.sync("./src/**/*.js"),
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
